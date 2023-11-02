7bc84ed7ca93be1440798f0760d5522a5bbe78eb
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-11-02 13:39:15 -0500

    hllo

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
 const Location = require('./location');
 const Traveller = require('./traveller');
 const Trips = require('./trips');
+
+travellers.hasMany(location, {
+    foreignKey: 'traveller_id',
+    onDelete: 'CASCADE',
+})
+
+module.exports = {Location, Traveller, Trips};
